c27c5a7ec57ae44c3ee153039eb88531c319c821
shameema0 <66423999+shameema0@users.noreply.github.com>
2020-06-06 11:32:40 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+dict = {"disha" : 20,"anisha" : 19,"diya" : 18, "anu" : 17}
+print("The dictionary before performing remove is : " + str(dict))
+del dict['diya']
+print("The dictionary after remove is : " + str(dict))
